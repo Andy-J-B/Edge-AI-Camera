c0325b91d33fb8da550516db38d495aca549361d
Andy-J-B <080.abae@gmail.com>
2026-06-16 21:54:33 -0700

    Parameterize and implement line_buffer
    Convert line_buffer to a parameterized module (WIDTH, DATA_WIDTH) and implement an internal line buffer array with a pointer. Add synchronous pointer logic (reset and increment on in_valid with wraparound) and read/write behavior that updates pixel_out from the buffer and writes pixel_in into the buffer. Also add .nvim to .gitignore.

--- hardware/rtl/line_buffer.v
+++ hardware/rtl/line_buffer.v
@@ -1,12 +1,29 @@
-module line_buffer ( 
-  input clk, rst_n,
-  input in_valid,
-  input [7:0] pixel_in,
-  output reg[7:0] pixel_out
+module line_buffer #(
+    parameter int WIDTH = 640,
+    parameter int DATA_WIDTH = 8
+) (
+    input clk,
+    rst_n,
+    input in_valid,
+    input [7:0] pixel_in,
+    output reg [7:0] pixel_out
 );
-  
-  parameter WIDTH = 640;
-  parameter DATA_WIDTH = 8;
+  reg [$clog2(WIDTH)-1:0] pointer;
 
+  reg [DATA_WIDTH-1:0] line_buffer[WIDTH];
+
+  always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) pointer <= 0;
+    else if (in_valid)
+      if (pointer == (WIDTH - 1)) pointer <= 0;
+      else pointer <= pointer + 1;
+  end
+  reg pixel_read;
+  always @(posedge clk) begin
+    if (in_valid) begin
+      pixel_out <= line_buffer[pointer];
+      line_buffer[pointer] <= pixel_in;
+    end
+  end
 endmodule
 
